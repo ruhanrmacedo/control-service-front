78aeadd8207d6e78341f632dc4b92ac2d747d123
Ruhan <ruhan_350@hotmail.com>
2025-09-22 00:18:39 -0300

    Altera footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  year = new Date().getFullYear();
 
+  links = {
+    linkedin: 'https://www.linkedin.com/in/ruhan-macedo-4a25a3182/',
+    instagram: 'https://www.instagram.com/ruhanrmacedo/',
+    whatsapp: 'https://wa.me/5548991351845?text=Ol%C3%A1!%20Vim%20pelo%20site%20Control%20Service.'
+  };
 }
